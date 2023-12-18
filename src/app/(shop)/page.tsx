564793847ca7fd69dc5d9b9b9fd83904df81906b
Marcos923 <marcos.daut@gmail.com>
2023-12-18 12:06:46 -0300

    agregando componente title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,12 +1,11 @@
+import Title from "@/components/ui/title/Title";
 import { titleFont } from "@/config/font";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <h1>Hola mundo</h1>
-      <h1 className={`${titleFont.className} font-bold`}>Hola mundo</h1>
-      <h1 className={`${titleFont.className} `}>Hola mundo</h1>
-    </div>
+    <>
+      <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+    </>
   );
 }
