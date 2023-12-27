4f9490b55d487c3e3a70cc8de9a1ff9ae728d387
Marcos923 <marcos.daut@gmail.com>
2023-12-27 11:10:14 -0300

    configuracion inicial de seed

---
+++ src/seed/seed-database.ts
@@ -0,0 +1,12 @@
+import { initialData } from "./seed";
+
+async function main() {
+  console.log(initialData);
+
+  console.log("Seed Ejecutado correctamente");
+}
+
+(() => {
+  if (process.env.NODE_ENV === "production") return;
+  main();
+})();
