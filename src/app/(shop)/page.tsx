f059c88fb3782d3129885287ff327e74e32ae1c5
Marcos923 <marcos.daut@gmail.com>
2023-12-28 17:22:33 -0300

    cargando productos de base de datos y mostrandolos en pantalla

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,10 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import ProductGrid from "@/components/products/product-grid/ProductGrid";
 import Title from "@/components/ui/title/Title";
-import { initialData } from "@/seed/seed";
 
-const products = initialData.products;
+export default async function Home() {
+  const { products } = await getPaginatedProductsWithImages();
 
-export default function Home() {
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
