87a95deb0e83a1cabc2d0c733616953f08cb77dc
Marcos923 <marcos.daut@gmail.com>
2024-01-03 21:22:53 -0300

    agregando funcionalidad al html de la paginacion

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import Link from "next/link";
-import {
-  IoChevronBackCircleOutline,
-  IoChevronBackOutline,
-  IoChevronForwardOutline,
-} from "react-icons/io5";
+import { usePathname, useSearchParams } from "next/navigation";
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 
 interface Props {
   totalPages: number;
 }
 
 export const Pagination = ({ totalPages }: Props) => {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) ?? 1;
+
+  const createPageUrl = (pageNumber: number | string) => {
+    const params = new URLSearchParams(searchParams);
+
+    if (pageNumber === "...") {
+      return `${pathname}?${params.toString()}`;
+    }
+
+    if (+pageNumber === 0) {
+      return `${pathname}`; // href ="/kid"
+    }
+
+    if (+pageNumber > totalPages) {
+      // Next >
+      return `${pathname}?${params.toString()}`;
+    }
+
+    params.set("page", pageNumber.toString());
+    return `${pathname}?${params.toString()}`;
+  };
+
   return (
     <div className="flex text-center justify-center mt-10 mb-10 ">
       <nav aria-label="Page navigation example">
@@ -17,7 +40,7 @@ export const Pagination = ({ totalPages }: Props) => {
           <li className="page-item disabled">
             <Link
               className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#"
+              href={createPageUrl(currentPage - 1)}
               aria-disabled="true"
             >
               <IoChevronBackOutline size={30} />
@@ -48,9 +71,25 @@ export const Pagination = ({ totalPages }: Props) => {
             </a>
           </li>
           <li className="page-item">
-            <Link
+            <a
               className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
               href="#"
+            >
+              ...
+            </a>
+          </li>
+          <li className="page-item">
+            <a
+              className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+              href="#"
+            >
+              50
+            </a>
+          </li>
+          <li className="page-item">
+            <Link
+              className="page-link relative block py-1.5 px-3  border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+              href={createPageUrl(currentPage + 1)}
             >
               <IoChevronForwardOutline size={30} />
             </Link>
